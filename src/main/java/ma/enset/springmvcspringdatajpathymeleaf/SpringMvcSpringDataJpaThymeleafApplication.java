c646afdec4fe00dcbfa07911445c3656eaa87a4f
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2025-03-17 14:29:05 +0000

    other thymeleaf pages with Bootstrap styles

--- src/main/java/ma/enset/springmvcspringdatajpathymeleaf/SpringMvcSpringDataJpaThymeleafApplication.java
+++ src/main/java/ma/enset/springmvcspringdatajpathymeleaf/SpringMvcSpringDataJpaThymeleafApplication.java
@@ -1,13 +1,45 @@
 package ma.enset.springmvcspringdatajpathymeleaf;
 
+import ma.enset.springmvcspringdatajpathymeleaf.entities.Patient;
+import ma.enset.springmvcspringdatajpathymeleaf.repository.PatientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class SpringMvcSpringDataJpaThymeleafApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringMvcSpringDataJpaThymeleafApplication.class, args);
+    }
+    //@Bean
+    CommandLineRunner commandLineRunner(PatientRepository patientRepository) {
+        return args -> {
+            patientRepository.save(Patient.builder()
+                            .nom("Moanim")
+                            .dateNaissance(new Date())
+                            .malade(true)
+                            .score(122)
+
+                    .build());
+            patientRepository.save(Patient.builder()
+                    .nom("Amine")
+                    .dateNaissance(new Date())
+                    .malade(true)
+                    .score(45)
+
+                    .build());
+            patientRepository.save(Patient.builder()
+                    .nom("Mouad")
+                    .dateNaissance(new Date())
+                    .malade(true)
+                    .score(11)
+
+                    .build());
+        };
     }
 
 }
