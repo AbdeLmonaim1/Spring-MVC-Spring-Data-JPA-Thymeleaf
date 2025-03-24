c2bd3c6f3b81db83752a82cece69b5e8e813cd41
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2025-03-24 14:10:46 +0000

    Define users and their roles in InMemoryUserDetailsManager Strategy

--- src/main/java/ma/enset/springmvcspringdatajpathymeleaf/SpringMvcSpringDataJpaThymeleafApplication.java
+++ src/main/java/ma/enset/springmvcspringdatajpathymeleaf/SpringMvcSpringDataJpaThymeleafApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Date;
 
@@ -40,6 +42,10 @@ public class SpringMvcSpringDataJpaThymeleafApplication {
 
                     .build());
         };
+    }
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
--- src/main/java/ma/enset/springmvcspringdatajpathymeleaf/security/SecurityConfig.java
+++ src/main/java/ma/enset/springmvcspringdatajpathymeleaf/security/SecurityConfig.java
@@ -1,5 +1,32 @@
 package ma.enset.springmvcspringdatajpathymeleaf.security;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+
 public class SecurityConfig {
-    
+    private PasswordEncoder passwordEncoder;
+    public SecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+    public InMemoryUserDetailsManager userDetailsManager() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("Monaim").password(passwordEncoder.encode("monaim123")).roles("USER", "ADMIN").build(),
+                User.withUsername("Mouad").password(passwordEncoder.encode("mouad123")).roles("USER").build(),
+                User.withUsername("Amine").password(passwordEncoder.encode("amine123")).roles("USER").build()
+        );
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.build();
+    }
 }
